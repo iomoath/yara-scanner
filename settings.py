7903785912c4434ba96a59dcfd953ce2f6bca6e8
Moaaz <moath@vegalayer.com>
2020-11-21 00:43:24 +0200

    Use Neo23x0 repo to fetch rules

--- settings.py
+++ settings.py
@@ -1,10 +1,16 @@
 ################ Internal settings, usually remains the same! ################
 tmp_directory = 'tmp'
+
+# Compiled rules directory
 yara_rules_directory = 'yara-rules'
-yara_rules_repo_url = 'https://github.com/Yara-Rules/rules'
+
+# Uncompiled rules directory (Src). Yara rules in this diectory will be compiled automatically when start
+yara_rules_src_directory = 'yara-rules-rc'
+
+yara_rules_repo_url = 'https://github.com/Neo23x0/signature-base'
 yara_rules_repo_download_url = yara_rules_repo_url + '/archive/master.zip'
-yara_rules_zipped_name = 'yara-rules.zip'
-yara_rules_directory_name_in_zip = 'rules-master'
+yara_rules_zipped_name = 'signature-base.zip'
+yara_rules_directory_name_in_zip = 'signature-base-master/yara'
 yara_matching_timeout = 30 # timeout in seconds
 max_file_size = 6777216 # Max file size 16 MB
 debug_log_enabled = False
