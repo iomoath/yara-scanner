ba3f142d94ab204baf6a00e7a6635ce3541f02f7
Moaaz <moath@vegalayer.com>
2020-11-28 20:32:30 +0200

    Update exclusions list

--- exclude.py
+++ exclude.py
@@ -1,5 +1,20 @@
 import os
+import settings
 
-################ Excluded paths from scan ################
+################ Exclude paths from scan ################
 excluded_path_list = []
 
+# Use double backslash for windows path's
+# Example: excluded_path_list.append("C:\\windows\\temp")
+
+# Exclude yara rules directory path by default
+yara_rules_dir = os.path.join(os.getcwd(), settings.yara_rules_directory)
+excluded_path_list.append(yara_rules_dir)
+
+
+# Recommended exclusions
+excluded_path_list.append('C:\\$Recycle.Bin\\')
+excluded_path_list.append('C:\\System Volume Information\\DFSR')
+
+################ Exclude files by extension ################
+excluded_file_extensions = [".yar", ".log", ".chk", ".sdb", ".jdb", ".pat", ".jrs", ".dit", ".pol", ".mdb", ".dns", ".admx", ".adml", ".adm", ".edb", ".db", ".evtx"]
