b352c1f5b28c59c29a783041d204c8045c65fc0e
Moaaz <moath@vegalayer.com>
2020-12-11 20:52:23 +0200

    Added support for adding multiple yara signatures sources

--- yara_updater.py
+++ yara_updater.py
@@ -32,14 +32,14 @@ def init_directories():
         os.makedirs(settings.yara_rules_directory)
 
 
-def find_yara_files():
+def find_yara_files(path):
     """
-    Search for Yara-Rules files path(s) defined in given list within directory $tmp_directory/rules-master
+    Search for Yara-Rules files path(s) in a given directory path
     :return: List contains yara rules path(s)
     """
     rule_path_list = []
 
-    rules_dir_absolute_path = os.path.abspath(os.path.join(settings.tmp_directory, settings.yara_rules_directory_name_in_zip))
+    rules_dir_absolute_path = os.path.abspath(path)
     file_list = common_functions.get_file_set_in_dir(rules_dir_absolute_path, True)
 
     for file_path in file_list:
@@ -63,38 +63,58 @@ def update():
     Update yara-rules in yara_rules_directory by downloading latest files from yara rules github repo yara_rules_repo_url
     :return: True on success, False on fail
     """
+
+    logger.log_info('Started Yara rules update', module_name)
+    logger.log_debug('Initializing directories', module_name)
+    print('[+] Started Yara rules update')
+    print('[+] Initializing directories..')
+    init_directories()
+
     try:
-        logger.log_info('Started Yara-Rules update', module_name)
-        logger.log_debug('Initializing directories', module_name)
-        print('[+] Started Yara-Rules update')
-        print('[+] Initializing directories..')
-        init_directories()
+        for entry in settings.yara_rules_repo_download_urls:
+            try:
+                if not entry['enabled']:
+                    continue
 
-        logger.log_debug('Fetching latest Yara-Rules from {}'.format(settings.yara_rules_repo_download_url), module_name)
-        print('[+] Fetching latest Yara-Rules from {}'.format(settings.yara_rules_repo_download_url))
-        save_path = os.path.join(settings.tmp_directory, settings.yara_rules_zipped_name)
+                logger.log_debug('Fetching signatures from {}'.format(entry['download_url']), module_name)
+                print('[+] Fetching signatures from {}'.format(entry['download_url']))
 
-        common_functions.download(settings.yara_rules_repo_download_url, save_path)
-        common_functions.extract_zip(save_path, settings.tmp_directory)
+                if entry['file_type'] == 'zip':
+                    file_name = entry['name'] + '.zip'
+                    save_path = os.path.join(settings.tmp_directory, file_name)
+                    common_functions.download(entry['download_url'], save_path)
+                    common_functions.extract_zip(save_path, settings.tmp_directory)
+                    rules_dir_absolute_path = os.path.abspath(
+                        os.path.join(settings.tmp_directory, entry['yara_rules_directory_name_in_zip']))
+                    yara_rule_path_list = find_yara_files(rules_dir_absolute_path)
 
-        yara_rule_path_list = find_yara_files()
+                    if yara_rule_path_list is None or len(yara_rule_path_list) <= 0:
+                        logger.log_error(
+                            'Could not find any yara files that matches the specified in $yara_rules_file_list',
+                            module_name)
+                        print(
+                            '[-] ERROR: Could not find any yara files that matches the specified in $yara_rules_file_list')
+                        continue
 
-        if yara_rule_path_list is None or len(yara_rule_path_list) <= 0:
-            logger.log_error('Could not find any yara files that matches the specified in $yara_rules_file_list', module_name)
-            print('[-] ERROR: Could not find any yara files that matches the specified in $yara_rules_file_list')
-            return False
+                    logger.log_debug('Compiling rules..', module_name)
+                    print('[+] Compiling rules..')
+                    common_functions.compile_yara_rules(yara_rule_path_list, settings.yara_rules_directory)
+                elif entry['file_type'] == 'yara':
+                    file_name = entry['name'] + '.yar'
+                    save_path = os.path.join(settings.tmp_directory, file_name)
+                    common_functions.download(entry['download_url'], save_path)
 
-        logger.log_debug('Compiling rules', module_name)
-        print('[+] Compiling rules..')
-        common_functions.compile_yara_rules(yara_rule_path_list, settings.yara_rules_directory)
-
+                    logger.log_debug('Compiling rules..', module_name)
+                    print('[+] Compiling rules..')
+                    common_functions.compile_yara_rules([save_path], settings.yara_rules_directory)
+            except Exception as e:
+                print('[-] ERROR fetching rules from {} : {}'.format(entry['name'], e))
+                logger.log_error(e, module_name)
+                continue
+    finally:
         logger.log_debug('Cleaning up', module_name)
         print('[+] Cleaning up..')
         clean_up()
         logger.log_info('Update complete', module_name)
         print('[+] Update complete.')
         return True
-    except Exception as e:
-        print('[-] ERROR: {}'.format(e))
-        logger.log_error(e, module_name)
-        return False
